9ea1022af977f303df68cb09ac23ad6475bbebbe
flum1025 <flum.1025@gmail.com>
2016-08-11 16:25:16 +0900

    optparseを追加

--- main.rb
+++ main.rb
@@ -1,4 +1,12 @@
 # Coding: UTF-8
 require_relative 'core/core'
+require 'optparse'
 
-Flumtter.start debug: true#, non_stream: true, non_read_buf: true
+opt = OptionParser.new
+options = {}
+opt.on('-d', '--debug'){|v|options[:debug] = true}
+opt.on('-s', '--stream'){|v|options[:non_stream] = true}
+opt.on('-r', '--read_buf'){|v|options[:non_read_buf] = true}
+opt.parse!(ARGV)
+
+Flumtter.start options
