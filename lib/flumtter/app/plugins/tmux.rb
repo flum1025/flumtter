ba0001849ade6d46102be8f77d9688159167ed74
flum1025 <flum.1025@gmail.com>
2017-03-01 14:15:29 +0900

    support multi column

---
+++ lib/flumtter/app/plugins/tmux.rb
@@ -0,0 +1,48 @@
+module Flumtter
+  plugin do
+    def pane_setting(name)
+      {
+        "#{name}"=> [
+          "flumtter -n #{name}"
+        ]
+      }
+    end
+
+    def mux_setting(panes)
+      {
+        "name"=> "flumtter",
+        "root"=> "~/",
+        "windows"=> [
+          {
+            "window1"=> {
+              "layout"=> "even-horizon",
+              "panes"=> panes
+            }
+          }
+        ]
+      }
+    end
+
+    def set_yml(setting)
+      File.write(File.join(Dir.home, ".tmuxinator", "flumtter.yml"), YAML.dump(setting))
+    end
+
+    add_opt do |opt, options|
+      opt.on('--tmux', 'enable tmux mode'){|v|options[:tmux] = v}
+      opt.on('--names=V,V,...', Array, 'set account names with tmux'){|v| options[:names] = v}
+    end
+
+    run_opt(:tmux) do |v, options|
+      require 'yaml'
+      accounts = if options[:names] && !options[:names].empty?
+        options[:names]
+      else
+        AccountSelector.list.map{|a|a.screen_name}
+      end
+      setting = mux_setting(accounts.map{|a|pane_setting(a)})
+      set_yml setting
+      `tmuxinator start flumtter`
+      exit
+    end
+  end
+end
